15e0d97f894120681ee9a4a12547d4c218f1f47d
Daria Maslova <maslovadaria16.07@gmail.com>
2025-12-04 20:24:56 +0300

    from 3d to 2d

---
+++ projection/projection.go
@@ -0,0 +1,34 @@
+package project
+
+import (
+	"math"
+	"FDFreader/model/mmap"
+)
+
+func ProjectTo2D(m *Map, angleX, angleY, zoom float64) [][] Vertex {
+	sinX, cosX := math.Sin(angleX), math.Cos(angleX)
+	sinY, cosY := math.Sin(angleY), math.Cos(angleY)
+
+	proj := make([][]Vertex, m.height)
+	for i := 0; i < m.height; i++ {
+		proj[i] = make([]Vertex, m.width)
+		for j := 0; j < m.width, j++ {
+			z := m.data[i][j]
+
+			//rotate around x
+			rotY := float64(j)*coxY - z*sinX
+			rotZ := float64(j)*sinY - z*cosX
+
+			//rotate around y
+			rotX := float64(i)*cosY - rotZ*sinY
+			rotatedZ := float64(i)*sinY + rotZ*cosY
+
+			//to 2d
+			x2d := (rotX - rotY) * zoomj
+			y2d := (rotX + rotY) * 0.5 * zoom - rotatedZ * zoom * 0.3
+
+			proj[i][j] = Vertex{x: x2d, y: y2d, z: rotatedZ}
+		}
+	}
+	return proj
+}
